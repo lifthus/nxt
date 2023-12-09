299cf075ae29f876e850737284eb993d8264323f
lifthus <lifthus531@gmail.com>
2023-12-09 12:09:52 +0900

    ch15

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -110,3 +110,25 @@ export async function deleteInvoice(id: string) {
   }
   revalidatePath(`/dashboard/invoices`);
 }
+
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (err) {
+    if (err instanceof AuthError) {
+      switch (err.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw err;
+  }
+}
